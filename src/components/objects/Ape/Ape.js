9dba3c529e0bd3235a0a1a88661c0d50d9704837
Donovan Kreul <37644774+navonodk@users.noreply.github.com>
2023-05-02 13:40:48 -0400

    fixed Ape scale, position, and rotation

--- src/components/objects/Ape/Ape.js
+++ src/components/objects/Ape/Ape.js
@@ -1,4 +1,4 @@
-import { Group } from 'three';
+import { Group, Vector3 } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import MODEL from './Ape.gltf';
 
@@ -10,6 +10,9 @@ class Ape extends Group {
         const loader = new GLTFLoader();
 
         this.name = 'ape';
+        this.scale.set(0.1, 0.1, 0.1);
+        this.position.set(0,-0.7,0);
+        this.rotation.y += Math.PI;
 
         loader.load(MODEL, (gltf) => {
             this.add(gltf.scene);
